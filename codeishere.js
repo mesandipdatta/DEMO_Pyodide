91b58e9cdf4e293d34e0b928c0fccaf725aecbb0
Sandip Datta <sandip.ddb@gmail.com>
2024-05-25 11:04:07 +0530

    updates

--- codeishere.js
+++ codeishere.js
@@ -1,6 +1,8 @@
 // Load and initialize Pyodide
 async function main() {
-    let pyodide = await loadPyodide();
+    let pyodide = await loadPyodide({
+      indexURL: "https://cdn.jsdelivr.net/pyodide/v0.21.0/full/"
+    });
   
     // Function to run the Python script
     async function translateEnglish() {
